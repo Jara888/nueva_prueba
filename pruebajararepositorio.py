018e9d1f9a19cecf3e66ea8577e394cd213e2b00
Jara888 <jujagu20@petalmail.com>
2022-10-12 14:14:03 -0500

    este archivo es una prueba más by jaras

---
+++ pruebajararepositorio.py
@@ -0,0 +1,54 @@
+print("Hola Mundo Tripulantes, Bienvenidos Mintic 2022")
+n1=1
+n2=3
+x=100
+
+print(n1)
+print(n2)
+print(x)
+
+decimal=2.5
+caracteres='UNAB'
+print(decimal)
+print(caracteres)
+
+d,f,nombre=50,20,'reyner'
+
+suma=d+f
+resta=d-f
+producto=d*f
+div=d/f
+exponente=n2**n2
+modulo=20%2
+print('la suma es :',suma)
+print('la resta es :',resta)
+print('la producto es :',producto)
+print('la div es :',div)
+print('la modulo es :',modulo)
+print('la exponente es :',exponente)
+
+print(type(decimal))
+print(type(div))
+print(type(suma))
+print(type(nombre))
+
+#comentarios linea
+"""
+el bloque
+
+"""
+
+print("Sumar Dos NUmeros")
+num1=float(input('Ingrese Primer Numero '))
+num2=input('Ingrese Segundo Numero ')
+c=int(input('ingrese valor entero'))
+suma=(num1+float(num2))*c
+
+print("La suma es :", suma)
+print("eres hiper inteligente")
+nombre=input()
+print(c)
+
+
+
+
